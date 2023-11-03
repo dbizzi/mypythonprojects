54b45344c17ef1705c484d29726aa94991aa6f52
dbizzi <danielbizzi@gmail.com>
2023-11-03 16:34:30 +0000

    tip calculator

---
+++ lifeInWeeks.py
@@ -0,0 +1,6 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+yearsLeft = 90 - int(age)
+weeksLeft = yearsLeft * 52
+print(f"You have {weeksLeft} weeks left.")
